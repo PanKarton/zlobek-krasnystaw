1958aeb3f3904d093947e8a5fd2c2bc4b76af905
arek <jack-swad@wp.pl>
2022-10-10 19:43:47 +0100

    added react-hook-form

--- Components/Molecules/ContactForm/ContactForm.js
+++ Components/Molecules/ContactForm/ContactForm.js
@@ -2,14 +2,25 @@ import { FormButton } from 'Components/Atoms/FormButton/FormButton';
 import { StyledInput } from 'Components/Atoms/FormInput/FormInput';
 import { StyledTextArea } from 'Components/Atoms/FormTextArea/FormTextArea';
 import { StyledForm } from './ContactForm.styles';
+import { useForm } from 'react-hook-form';
 
-const ContactForm = props => (
-  <StyledForm>
-    <StyledInput name="name" id="name" type="text" placeholder="Twoje imię" />
-    <StyledInput name="email" id="email" type="text" placeholder="Twój email" />
-    <StyledTextArea name="message" id="message" type="text" placeholder="W czym możemy pomóc?" />
-    <FormButton type="submit">Wyślij</FormButton>
-  </StyledForm>
-);
+const ContactForm = props => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      <StyledInput {...register('user_name')} id="name" type="text" placeholder="Twoje imię" />
+      <StyledInput {...register('user_email')} id="email" type="email" placeholder="Twój email" />
+      <StyledTextArea
+        {...register('message')}
+        id="message"
+        type="text"
+        placeholder="W czym możemy pomóc?"
+      />
+      <FormButton type="submit">Wyślij</FormButton>
+    </StyledForm>
+  );
+};
 
 export default ContactForm;
