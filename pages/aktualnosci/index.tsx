e8b9c300bc2eb8732533af38195a6b050e28ef2f
arek <jack-swad@wp.pl>
2022-10-29 21:53:10 +0100

    getSTaticProps env variable fetch to component test

--- pages/aktualnosci/index.tsx
+++ pages/aktualnosci/index.tsx
@@ -1,10 +1,22 @@
 import NewsListSection from 'Components/Organisms/NewsListSection/NewsListSection';
 import SecondaryTemplate from 'Components/Templates/SecondaryTemplate/SecondaryTemplate';
+import { getEnv } from 'helpers/getEnv';
 
-const News = () => (
+const News = (googleApiKey: string) => (
   <SecondaryTemplate heading="Nasze nowości">
+    <p>{googleApiKey}</p>
     <NewsListSection />
   </SecondaryTemplate>
 );
 
 export default News;
+
+export async function getStaticProps() {
+  const googleApiKey = getEnv(process.env.GOOGLE_MAPS_API_KEY);
+
+  return {
+    props: {
+      googleApiKey,
+    },
+  };
+}
