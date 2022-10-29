44f6320ff3e107eec2269c7807da0ac776d8ac64
arek <jack-swad@wp.pl>
2022-10-29 20:37:43 +0100

    removed pl from next.config

--- pages/rekrutacja/index.tsx
+++ pages/rekrutacja/index.tsx
@@ -1,9 +1,10 @@
 import SecondaryTemplate from 'Components/Templates/SecondaryTemplate/SecondaryTemplate';
 
 const Recruitment = () => (
-  <SecondaryTemplate heading="Rekrutacja pociech">
-    <section></section>
-  </SecondaryTemplate>
+  // <SecondaryTemplate heading="Rekrutacja pociech">
+  //   <section></section>
+  // </SecondaryTemplate>
+  <div></div>
 );
 
 export default Recruitment;
