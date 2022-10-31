288057fb09e230976019bac66bf1a50064141940
arek <jack-swad@wp.pl>
2022-10-31 18:38:14 +0000

    test

--- pages/oplaty/index.tsx
+++ pages/oplaty/index.tsx
@@ -4,11 +4,14 @@ import { getEnvVariable } from 'helpers/getEnvVariable';
 
 type Props = {
   googleApiKey: string;
+  value: string;
 };
 
-const Contact = ({ googleApiKey }: Props) => (
+const Contact = ({ googleApiKey, value }: Props) => (
   <SecondaryTemplate googleApiKey={googleApiKey} heading="Informacje o opłatach">
-    <section></section>
+    <section>
+      <p>{value}</p>
+    </section>
   </SecondaryTemplate>
 );
 
@@ -18,15 +21,19 @@ export async function getStaticProps() {
   const nodeEnv = getEnv('NODE_ENV');
 
   let googleApiKey = '';
+  let value = '';
   if (nodeEnv === 'development') {
     googleApiKey = getEnvVariable(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
+    value = getEnvVariable(process.env.NEXT_PUBLIC_A);
   } else if (nodeEnv === 'production') {
     googleApiKey = getEnvVariable(process.env.GOOGLE_MAPS_API_KEY);
+    value = getEnvVariable(process.env.A);
   }
 
   return {
     props: {
       googleApiKey,
+      value,
     },
   };
 }
