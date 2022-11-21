e2b160a54e32458543744cde3574a007f128ba3d
arek <jack-swad@wp.pl>
2022-11-21 20:58:33 +0000

    addeed getStaticProps to newsList site

--- pages/aktualnosci/index.tsx
+++ pages/aktualnosci/index.tsx
@@ -1,7 +1,12 @@
 import { NewsListSection } from 'modules/aktualnosci/NewsListSection/NewsListSection';
 import { SecondaryTemplate } from 'Components/Templates/SecondaryTemplate/SecondaryTemplate';
+import {Props} from '../../types/newsPosts';
+import { GetStaticProps } from 'next';
 
-const News = () => {
+
+const News = ({ posts }: Props) => {
+  console.log(posts);
+
   return (
     <SecondaryTemplate heading="Nasze nowości">
       <NewsListSection />
@@ -9,3 +14,15 @@ const News = () => {
   );
 };
 export default News;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const res = await fetch('http://localhost:1337/api/news-posts');
+    const posts = await res.json();
+  
+    return {
+      props: {
+        posts,
+      },
+    };
+
+}
---
+++ types/newsPosts.ts
@@ -0,0 +1,80 @@
+// Types generated with https://app.quicktype.io/
+
+export type Props = {
+  posts: Welcome;
+};
+
+export interface Welcome {
+  data: WelcomeDatum[];
+  meta: Meta;
+}
+
+export interface WelcomeDatum {
+  id: number;
+  attributes: PurpleAttributes;
+}
+
+export interface PurpleAttributes {
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  image: Image;
+}
+
+export interface Image {
+  data: ImageDatum[] | null;
+}
+
+export interface ImageDatum {
+  id: number;
+  attributes: FluffyAttributes;
+}
+
+export interface FluffyAttributes {
+  name: string;
+  alternativeText: null;
+  caption: null;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewURL: null;
+  provider: string;
+  providerMetadata: null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Formats {
+  thumbnail: Small;
+  small: Small;
+}
+
+export interface Small {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path: null;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+}
+
+export interface Meta {
+  pagination: Pagination;
+}
+
+export interface Pagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
