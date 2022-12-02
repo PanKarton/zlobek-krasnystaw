33c14615521fe63d78734b2898859f81d4e81ff0
arek <jack-swad@wp.pl>
2022-12-02 21:18:47 +0000

    added button to display all months when there are more than 5 months

--- modules/aktualnosci/ArchivesList/ArchivesList.tsx
+++ modules/aktualnosci/ArchivesList/ArchivesList.tsx
@@ -4,27 +4,37 @@ import { TextButton } from 'modules/aktualnosci/TextButton/TextButton';
 import { ArchivesListItem } from '../ArchivesListItem/ArchivesListItem';
 import { StyledWrapper } from './ArchivesList.styles';
 import { useNewPosts } from 'providers/NewsPostsProvider';
+import { useState } from 'react';
 
 export const ArchivesList = () => {
+  const [areAllVisible, setAreAllVisible] = useState(false);
   const { archivesErrorMessage } = useNewPosts();
 
   const RELEASE_DATE = new Date(getEnvVariable(process.env.NEXT_PUBLIC_RELEASE_DATE));
   const monthsData = getMonthsSinceDate(RELEASE_DATE);
 
+  const months = areAllVisible ? monthsData : monthsData.slice(0, 5);
+  const isButtonVisible = monthsData.length >= 6;
+
   return (
     <StyledWrapper>
-      <div className="archives">
-        <h3 className="archives__heading">Archiwum</h3>
-        <ul className="archives__list">
-          {monthsData &&
-            monthsData.map((month, index) => (
-              <li key={index} className="archives__list-item">
-                <ArchivesListItem monthData={month} />
-              </li>
-            ))}
-        </ul>
-        {archivesErrorMessage && <p className="error-message">Ups, coś poszło nie tak. Spróbuj ponownie! :)</p>}
-      </div>
+      <h3 className="heading">Archiwum</h3>
+      <ul className="list">
+        {monthsData &&
+          months.map((month, index) => (
+            <li key={index} className="list-item">
+              <ArchivesListItem monthData={month} />
+            </li>
+          ))}
+      </ul>
+      {archivesErrorMessage && <p className="error-message">Ups, coś poszło nie tak. Spróbuj ponownie! :)</p>}
+      {isButtonVisible && (
+        <TextButton
+          className="load-btn"
+          handleClick={() => setAreAllVisible((prevValue) => !prevValue)}
+          text={areAllVisible ? 'Ukryj ponownie!' : 'Pokaż więcej!'}
+        />
+      )}
     </StyledWrapper>
   );
 };
