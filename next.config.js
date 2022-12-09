f1c9e8179dcbfb0b9cef3a1644750b8c778804c7
arek <jack-swad@wp.pl>
2022-12-09 18:14:45 +0000

    added image displaying on news post page

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // i18n: {
-  //   locales: ['pl'],
-  //   defaultLocale: 'pl',
-  // },
+  i18n: {
+    locales: ['pl'],
+    defaultLocale: 'pl',
+  },
   images: {
     domains: ['localhost'],
   },
