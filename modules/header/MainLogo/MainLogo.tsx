eac93894adf8fa54dc69087f459a0f05f7307b9e
unknown <jack-swad@wp.pl>
2023-01-14 20:30:18 +0100

    added sizes prop to main logo Image

--- modules/header/MainLogo/MainLogo.tsx
+++ modules/header/MainLogo/MainLogo.tsx
@@ -6,7 +6,7 @@ export const MainLogo = () => (
   <h1>
     <Link href="/">
       <div className="logo-img-wrapper">
-        <Image src="/images/main-logo.svg" alt="Logo żłobka z klockami" fill />
+        <Image src="/images/main-logo.svg" alt="Logo żłobka z klockami" fill sizes="(max-width: 1440px) 90px, 120px" />
       </div>
     </Link>
   </h1>
